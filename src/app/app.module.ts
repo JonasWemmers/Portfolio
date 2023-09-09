2462fd167252ef7a4d26adbffaa5b544b8e53203
Jonas Wemmers <jonas.wemmers@freenet.de>
2023-09-09 17:49:25 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeadSectionComponent } from './head-section/head-section.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsetComponent } from './skillset/skillset.component';
+import { MyworkComponent } from './mywork/mywork.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { PopupMenuComponent } from './popup-menu/popup-menu.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadSectionComponent,
+    AboutMeComponent,
+    SkillsetComponent,
+    MyworkComponent,
+    ProjectsComponent,
+    ContactFormComponent,
+    FooterComponent,
+    PopupMenuComponent,
+    ImpressumComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/popup-menu/popup-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-menu',
+  templateUrl: './popup-menu.component.html',
+  styleUrls: ['./popup-menu.component.scss']
+})
+export class PopupMenuComponent {
+
+}
